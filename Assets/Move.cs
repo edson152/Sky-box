719f2650812039d7b4db3db2706e7c5c1d104553
edson152 <71998101+edson152@users.noreply.github.com>
2023-08-23 06:44:45 +0800

    first comit

---
+++ Assets/Move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	CharacterController controller;     //declare character controller
+	    public float pushPower = 5.0F;      //power to push 
+	    public float speed = 6.0f;          //speed to move
+	    public float rotateSpeed=3.0f;      //speed to rotate
+	    void Awake() {
+		        controller = this.GetComponent<CharacterController>();  //get its character controller
+		    }
+	    void Update() {        
+		        transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * rotateSpeed, 0)); //rotate player when pressing directional key “←”“→”
+		        Vector3 forward = transform.TransformDirection(Vector3.forward);    //transform from self coordinate system to world coordinate system
+		        controller.SimpleMove(forward * speed*Input.GetAxis("Vertical"));   //move the player when pressing the directional key“↑”“↓”
+		    }
+	    void OnControllerColliderHit(ControllerColliderHit hit)     //called when hit the movable gameobject 当碰撞到可移动的物体时被调用
+	    {
+		        Rigidbody body = hit.collider.attachedRigidbody;      //get the rigidbody of hit GameObject (获取被碰撞的物体的刚体组件)
+		        if (body == null || body.isKinematic)    //if there is no rigidbody or it does not meet the laws of kinematics (如果物体没有刚体组件或不符合运动学定律就返回)
+			            return;
+		        if (hit.moveDirection.y < -0.3F)   //return if the y-axis component in the direction from center of capsule to touched point is less than -0.3(如果胶囊碰撞器中心到触碰点的方向在y轴的分量小于-0.3时返回)
+			            return;
+		        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z); //set the moving direction of hit gameobject(设置被碰撞的物体的移动方向)
+		        body.velocity = pushDir * pushPower;        //add velocity on hit gameObject(为被碰撞的物体施加速度)
+		    }
+}
---
+++ Assets/SkyDemo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyDemo : MonoBehaviour {
+
+	public float rotateSpeed = 15.0f; //set rotate speed of camera(设置摄像机的旋转速度
+	    private GameObject camera1;    //declare gameobject(声明游戏对象用来获取摄像机
+	    public Material[] First;                //declare material array used to place the sky boxes that need to be switched (声明材质数组用于放置需要切换的天空盒
+	    public Material[] Second;
+	    private int index;                      //first array index(第一个材质数组的索引
+	    private int deindex;                    //second array index(第一个材质数组的索引
+	    void Awake() {
+		        camera1 = transform.Find("Camera (1)").gameObject;
+		        //get the Camera(1) and transform to gameObject(获取挂载该脚本的游戏对象下名为“Camera （1）”的子对象并将其转换为GameObject类型
+		    }
+	    void Update () {
+		        this.transform.Rotate(new Vector3(0,rotateSpeed*Time.deltaTime,0)); 
+		        //rotate around y axis
+		        if (Input.GetKeyDown(KeyCode.Space)){   //monitor keyboard and judge if space is pressed(添加键盘监听，判断空格键是否被按下
+			            RenderSettings.skybox = First[index++%First.Length];   // modify the skybox material in light panel(修改Light面板中的Skybox材质
+			            camera1.transform.GetComponent<Skybox>().material = Second[deindex++ % Second.Length];   //modify the skybox in camera (修改摄像机上天空盒组件的Skybox材质
+			        }
+		    }
+}
